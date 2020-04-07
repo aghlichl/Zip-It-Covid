2316fd23eecd645d4da9ac0bb5bc50a55140120b
Ari <aurashghlichloo@gmail.com>
2020-04-07 10:44:17 -0700

    prop review

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return <div>Hi There!</div>;
+  const buttonText = "click me";
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail author="Ari" timeAgo="6:32pm" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Pari" timeAgo="2:51pm" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Fatollah" timeAgo="6:12am" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Ida" timeAgo="1:11am" />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
