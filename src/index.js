d16652f39e1d8e69164fcca54793720caedf2548
Ari <aurashghlichloo@gmail.com>
2020-04-11 16:26:22 -0700

    initial skeleton and terraformer geospatial repo

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
 
-const App = () => {
-  const buttonText = "click me";
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail author="Ari" timeAgo="6:32pm" />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail author="Pari" timeAgo="2:51pm" />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail author="Fatollah" timeAgo="6:12am" />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail author="Ida" timeAgo="1:11am" />
-      </ApprovalCard>
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      zipCode: 0,
+      county: "",
+    };
+  }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+  onInputChange(e) {
+    this.setState({ zipCode: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.onInputChange}></input>
+        <br></br>
+        <div>{this.state.zipCode}</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
